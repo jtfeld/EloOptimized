41d7e063750e17f48ce11f998043e597f772224e
jtfeld <jfeldblum@gmail.com>
2017-12-19 18:42:09 +0000

    couple more boilerplate package components.  remove if you don't want but hadley wickham says to do it so I went ahead and did it

---
+++ R/EloOptimized.R
@@ -0,0 +1,15 @@
+#' EloOptimized: ML fitting of Elo Scores
+#'
+#' @description This package implements the maximum likelihood methods for deriving Elo scores as published in Foerster, Franz et al. (2016).
+#' Chimpanzee females queue but males compete for social status. Scientific Reports 6, 35404, doi:10.1038/srep35404
+#'
+#'
+#' @section Primary functions:
+#' \code{\link{eloratingopt}},
+#' \code{\link{elo.model1}},
+#' \code{\link{elo.model3}},
+#' \code{\link{eloratingoptR}}
+#'
+#' @docType package
+#' @name EloOptimized
+NULL
